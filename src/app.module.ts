20031df71b39aa590017286c6a72c5f22c957210
Vahid Rezazadeh <vahid.rezazadeh1372@gmail.com>
2023-01-24 14:45:53 +0330

    Add Minio Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MinioClientModule } from './minio-client/minio-client.module';
 
 @Module({
-  imports: [],
+  imports: [MinioClientModule],
   controllers: [AppController],
   providers: [AppService],
 })
